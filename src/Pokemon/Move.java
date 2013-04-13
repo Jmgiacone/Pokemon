34069bac0ae3cbd92bc25a11b1db1b0c2891eae3
Jordan Giacone <Jmgiacone@comcast.net>
2013-04-12 21:20:39 -0500

    Turned Move into an enum. Will add moves soon

--- src/Pokemon/Move.java
+++ src/Pokemon/Move.java
@@ -1,95 +1,93 @@
 package Pokemon;
 
-public class Move
+public enum Move
 {
-    public static final Move 
-            TACKLE = new Move("Tackle","Normal", 50, 35, 100),
-            TAIL_WHIP = new Move("Tail Whip","Normal", 0, 20, 100, Stat.DEFENSE),
-            GROWL = new Move("Growl","Normal", 0, 40, 100, Stat.ATTACK),
-            SCRATCH = new Move ("Scratch","Normal", 40, 35, 100),
-            BUBBLE = new Move("Bubble", "Water", 20, 35, 100),
-            RAPID_SPIN = new Move("Rapid Spin", "Normal", 20, 40, 100),
-            WITHDRAW = new Move("Withdraw", "Water", 0, 40, 100, Stat.DEFENSE),
-            WATER_GUN = new Move("Water Gun", "Water", 40, 35, 100),
-            BITE = new Move("Bite", "Dark", 60, 25, 100),
-            EMBER = new Move("Ember", "Fire", 40, 25, 100),
-            SMOKE_SCREEN = new Move("SmokeScreen", "Normal", 0, 20, 100, Status.ACCURACY),
-            DRAGON_RAGE = new Move("Dragon Rage", "Dragon", 0, 10, 100),
-            SCARY_FACE = new Move("Scary Face", "Normal", 0, 10, 100),
-            FIRE_FANG = new Move("Fire Fang", "Fire", 65, 15, 95, Status.BURN),
-            LEECH_SEED = new Move("Leech Seed", "Grass", 0, 10, 90, Stat.HP),
-            VINE_WHIP = new Move("Vine Whip", "Grass", 35, 15, 100),
-            POISONPOWDER = new Move("Poison Powder", "PSN", 0, 35, 75, Status.PSN),
-            TAKE_DOWN = new Move("Take Down", "Normal", 90, 20, 85),
-            RAZOR_LEAF = new Move("Razor Leaf", "Grass", 55, 25, 95);
+    TACKLE("Tackle",Type.NORMAL, 50, 35, 100),
+    TAIL_WHIP("Tail Whip",Type.NORMAL, 0, 20, 100, Stat.DEFENSE),
+    GROWL("Growl",Type.NORMAL, 0, 40, 100, Stat.ATTACK),
+    SCRATCH ("Scratch",Type.NORMAL, 40, 35, 100),
+    BUBBLE("Bubble", Type.WATER, 20, 35, 100),
+    RAPID_SPIN("Rapid Spin", Type.NORMAL, 20, 40, 100),
+    WITHDRAW("Withdraw", Type.WATER, 0, 40, 100, Stat.DEFENSE),
+    WATER_GUN("Water Gun", Type.WATER, 40, 35, 100),
+    BITE("Bite", Type.DARK, 60, 25, 100),
+    EMBER("Ember", Type.FIRE, 40, 25, 100),
+    SMOKE_SCREEN("SmokeScreen", Type.NORMAL, 0, 20, 100, Status.ACCURACY),
+    DRAGON_RAGE("Dragon Rage", Type.DRAGON, 0, 10, 100),
+    SCARY_FACE("Scary Face", Type.NORMAL, 0, 10, 100),
+    FIRE_FANG("Fire Fang", Type.FIRE, 65, 15, 95, Status.BURN),
+    LEECH_SEED("Leech Seed", Type.GRASS, 0, 10, 90, Stat.HP),
+    VINE_WHIP("Vine Whip", Type.GRASS, 35, 15, 100),
+    POISONPOWDER("Poison Powder", Type.POISON, 0, 35, 75, Status.PSN),
+    TAKE_DOWN("Take Down", Type.NORMAL, 90, 20, 85),
+    RAZOR_LEAF("Razor Leaf", Type.GRASS, 55, 25, 95);
     
-    private String type, name;
-    private int power, pp, currentPP, currentPower, accuracy;
+    private String NAME;
+    private final Type TYPE;
+    private int POWER, pp, currentPP, currentPower, ACCURACY;
     private final Status SIDE_EFFECT;
     private final Stat STAT_LOWERED;
 
-    public Move(String Name, String Type, int Power, int Pp, int Accuracy)
+    Move(String name, Type type, int power, int pp, int accuracy)
     {
-        name = Name;
-        type = Type;
-        accuracy = Accuracy;
-        power = Power;
-        pp = Pp;
+        NAME = name;
+        TYPE = type;
+        ACCURACY = accuracy;
+        POWER = power;
+        this.pp = pp;
         currentPP = pp;
-        currentPower = power;
+        currentPower = POWER;
         SIDE_EFFECT = null;
         STAT_LOWERED = null;
     }
-    public Move(String name, String type, int power, int pp, int accuracy, Status s)
+
+    Move(String name, Type type, int power, int pp, int accuracy, Status s)
     {
-        this.name = name;
-        this.type = type;
-        this.accuracy = accuracy;
-        this.power = power;
+        NAME = name;
+        TYPE = type;
+        ACCURACY = accuracy;
+        POWER = power;
         this.pp = pp;
         currentPP = pp;
-        currentPower = power;
+        currentPower = POWER;
         SIDE_EFFECT = s;
         STAT_LOWERED = null;
     }
 
-    public Move(String name, String type, int power, int pp, int accuracy, Stat s)
+
+    Move(String name, Type type, int power, int pp, int accuracy, Stat s)
     {
-        this.name = name;
-        this.type = type;
-        this.accuracy = accuracy;
-        this.power = power;
+        NAME = name;
+        TYPE = type;
+        ACCURACY = accuracy;
+        POWER = power;
         this.pp = pp;
         currentPP = pp;
-        currentPower = power;
+        currentPower = POWER;
         SIDE_EFFECT = null;
         STAT_LOWERED = s;
     }
 
-    public Move(String name, String type, int power, int pp, int accuracy, Status status, Stat stat)
+
+    Move(String name, Type type, int power, int pp, int accuracy, Status status, Stat stat)
     {
-        this.name = name;
-        this.type = type;
-        this.accuracy = accuracy;
-        this.power = power;
+        NAME = name;
+        TYPE = type;
+        ACCURACY = accuracy;
+        POWER = power;
         this.pp = pp;
         currentPP = pp;
-        currentPower = power;
+        currentPower = POWER;
         SIDE_EFFECT = status;
         STAT_LOWERED = stat;
     }
-    public int power()
+
+    public int getPower()
     {
-        return power;
+        return POWER;
     }
     
-    public int attackAI(String pName)
-    {
-        System.out.printf("Wild %s used %s\n", pName, name);
-        return power;
-    }
-    
-    public void changePP(int PP)
+    public void setPP(int PP)
     {
         currentPP = PP;
     }
@@ -99,12 +97,12 @@ public class Move
         currentPP--;
     }
     
-    public int currentPP()
+    public int getCurrentPP()
     {
         return currentPP;
     }
     
-    public int totalPP()
+    public int getTotalPP()
     {
         return pp;
     }
@@ -121,17 +119,17 @@ public class Move
     
     public String getName()
     {
-        return name;
+        return NAME;
     }
     
-    public String getType()
+    public Type getType()
     {
-        return type;
+        return TYPE;
     }
     
     public int getAccuracy()
     {
-        return accuracy;
+        return ACCURACY;
     }
     public int getCurrentPower()
     {
