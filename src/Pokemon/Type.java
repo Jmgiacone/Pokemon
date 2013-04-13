7e0fbd30a149f7a1e29b3a3e73f8f96804fea92b
Jordan Giacone <Jmgiacone@comcast.net>
2013-04-12 21:21:30 -0500

    Added more types to the Type enum

--- src/Pokemon/Type.java
+++ src/Pokemon/Type.java
@@ -1,12 +1,5 @@
 package Pokemon;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Jordan
- * Date: 4/12/13
- * Time: 6:57 PM
- * To change this template use File | Settings | File Templates.
- */
 public enum Type
 {
     GRASS,
