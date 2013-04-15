8400f0b5e5e08ca2f798645c857ab2fc03aab5b0
Jordan Giacone <Jmgiacone@comcast.net>
2013-04-15 03:56:42 -0500

    Created a JUnit test file to make sure that all the methods are working

--- src/Pokemon/Tests/TypeTest.java
+++ src/Pokemon/Tests/TypeTest.java
@@ -1,22 +1,34 @@
 package Pokemon.Tests;
 
+import Pokemon.Type;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
 public class TypeTest
 {
+    @Test
     public void testIsSuperEffectiveAgainst() throws Exception
     {
-
+        assertFalse(Type.POISON.isSuperEffectiveAgainst(Type.STEEL));
+        assertTrue(Type.WATER.isSuperEffectiveAgainst(Type.FIRE));
+        assertTrue(Type.BUG.isSuperEffectiveAgainst(Type.GRASS));
+        assertFalse(Type.FIGHTING.isSuperEffectiveAgainst(Type.GHOST));
     }
 
+    @Test
     public void testIsNotVeryEffectiveAgainst() throws Exception
     {
 
     }
 
+    @Test
     public void testHasNoEffectOn() throws Exception
     {
 
     }
 
+    @Test
     public void testIsNormalAgainst() throws Exception
     {
 
