08d7fc21363483f8b2c26252e2a6060febc030f7
Jordan Giacone <Jmgiacone@comcast.net>
2013-04-17 21:07:52 -0500

    Writing tests for the GenderRatio enum to ensure it works properly

--- src/Pokemon/Tests/GenderRatioTest.java
+++ src/Pokemon/Tests/GenderRatioTest.java
@@ -1,5 +1,18 @@
 package Pokemon.Tests;
 
+import Pokemon.Gender;
+import Pokemon.Species;
+import Pokemon.Type;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
 public class GenderRatioTest
 {
+    @Test
+    public void getGender() throws Exception
+    {
+        Gender g = Species.GenderRatio.ONE_FEMALE_SEVEN_MALE.getGender();
+        g.name();
+    }
 }
