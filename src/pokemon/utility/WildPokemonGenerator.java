21986a8af3c7ac8a60c944fc43ea123e0bb612f5
Nolan Hoffmann <9oxsteelflux@gmail.com>
2013-05-14 09:58:33 -0500

    A basic class that generates a random pokemon

---
+++ src/pokemon/utility/WildPokemonGenerator.java
@@ -0,0 +1,24 @@
+package pokemon.utility;
+
+import pokemon.core.Pokemon;
+import pokemon.core.Species;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nolan
+ * Date: 5/14/13
+ * Time: 9:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WildPokemonGenerator {
+    //TODO - make enums for each route and what pokemon can appear where along with the level range
+    public static Pokemon generatePokemon() {
+        int rand = (int) (Math.random() * Species.values().length);
+        for(final Species s : Species.values()) {
+            if(Integer.parseInt(s.getDexNumber()) == rand) {
+                return new Pokemon(s);
+            }
+        }
+        return null;
+    }
+}
