9672c5ac717f4d6ed5829481a490b58010acefef
Nolan Hoffmann <9oxsteelflux@gmail.com>
2013-05-14 09:58:12 -0500

    Added the start to battling

--- src/pokemon/engine/Powerhouse.java
+++ src/pokemon/engine/Powerhouse.java
@@ -3,6 +3,7 @@ package pokemon.engine;
 import pokemon.core.Pokemon;
 import pokemon.core.Species;
 import pokemon.interactive.Player;
+import pokemon.utility.WildPokemonGenerator;
 
 import java.util.Scanner;
 
@@ -18,6 +19,7 @@ public class Powerhouse {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         final String[] starters = new String[] {"Charmander", "Bulbasaur", "Squirtle"};
+        boolean battle = false;
         print(Powerhouse.class.getSimpleName(), true);
         print("Please enter your players name: ", false); Player player = new Player(in.nextLine());
         print("Please type the name of the start pokemon you would like (Charmander, Bulbasaur, Squirtle): ", false);
@@ -32,7 +34,16 @@ public class Powerhouse {
         if(player.isPartyEmpty()) {
             print("Failed to add pokemon to your party, you must choose one of the three starter pokemon.", true);
         } else {
-            print("Player contents: " + player.toString(), false);
+            print("Player contents: \n" + player.toString(), true);
+        }
+
+        print("Would you like to battle?: ", false); battle = in.nextLine().equalsIgnoreCase("yes") ? true : false;
+        player.setBattleState(battle);
+        while(player.isInBattle()) {
+            final Pokemon wild = WildPokemonGenerator.generatePokemon();
+            System.out.println("Wild pokemon information: " + wild);
+            print("You encountered a " + wild.getName() + "!", true);
+            break;
         }
     }
 
