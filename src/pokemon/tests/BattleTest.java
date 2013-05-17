56820394628cc69154aa948f2a98486638c6cea1
Jordan Giacone <Jmgiacone@comcast.net>
2013-05-17 11:17:48 -0500

    More tests

--- src/pokemon/tests/BattleTest.java
+++ src/pokemon/tests/BattleTest.java
@@ -1,13 +1,19 @@
 package pokemon.tests;
 
 import org.junit.Test;
+import pokemon.core.Move;
+import pokemon.core.Pokemon;
+import pokemon.core.Species;
+import pokemon.engine.Battle;
+import pokemon.interactive.Player;
 
 public class BattleTest
 {
     @Test
     public void testCalculateDamage() throws Exception
     {
-
+        new Battle(null, null).calculateDamage(new Pokemon(Species.GLACEON, 75), Move.ICE_FANG, new Pokemon(Species.GARCHOMP, 75));
+        new Battle(null, null).calculateDamage(new Pokemon(Species.GARCHOMP, 78), Move.EARTHQUAKE, new Pokemon(Species.GLACEON, 75));
     }
 
     @Test
