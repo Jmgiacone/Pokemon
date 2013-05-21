f78c39671813da9b21848f70723efc223f228046
Jordan Giacone <Jmgiacone@comcast.net>
2013-05-20 23:43:18 -0500

    Added a toString()

--- src/pokemon/core/Stat.java
+++ src/pokemon/core/Stat.java
@@ -3,4 +3,9 @@ package pokemon.core;
 public enum Stat
 {
     HP, ATTACK,  DEFENSE, SP_ATTACK, SP_DEFENSE, SPEED;
+
+    public String toString()
+    {
+        return Character.toUpperCase(super.toString().charAt(0)) + super.toString().substring(1).toLowerCase();
+    }
 }
